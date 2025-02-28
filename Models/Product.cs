b2dcd3bd916979ec59dae8ac5e51ffd1edc277bd
Bobák Kornél <143836544+dianne-x@users.noreply.github.com>
2025-02-28 12:56:35 +0100

    Add project files.

---
+++ Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductAPI.Models;
+
+[Table("products")]
+public partial class Product
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("name")]
+    [StringLength(255)]
+    [Unicode(false)]
+    public string Name { get; set; } = null!;
+
+    [Column("description", TypeName = "text")]
+    public string Description { get; set; } = null!;
+
+    [Column("price", TypeName = "decimal(10, 2)")]
+    public decimal Price { get; set; }
+
+    [Column("image_name")]
+    [StringLength(255)]
+    [Unicode(false)]
+    public string ImageName { get; set; } = null!;
+}
---
+++ Models/ProductsDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductAPI.Models;
+
+public partial class ProductsDbContext : DbContext
+{
+    public ProductsDbContext()
+    {
+    }
+
+    public ProductsDbContext(DbContextOptions<ProductsDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=product_catalog;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__products__3213E83F88870D6D");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
